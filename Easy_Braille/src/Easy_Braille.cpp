8bf8ccbfce54768f41c368b1c5edd0e0685ba214
Jimenez-Eric <61160495+Jimenez-Eric@users.noreply.github.com>
2020-02-17 14:31:23 -0600

    Funciona el reproductor
    Ver como hacerlo que reprodusca la letra deceada

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -2,21 +2,38 @@
 #include <serial_mp3.h>
 
 
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(2, OUTPUT);
+
+  //Serial2.begin(9600);
+  mp3.begin(9600);
+  delay(500);
+ 
+  sendCommand(CMD_SEL_DEV, DEV_TF);
+  delay(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  reproducir();
-  Serial.println("La  suma de a + b es ");
-  Serial.println(c);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
+
+  char c = ' ';
+ 
+  // If there a char on Serial call sendMP3Command to sendCommand
+  if ( Serial.available() )
+  {
+    c = Serial.read();
+    sendMP3Command(c);
+  }
+ 
+  // Check for the answer.
+  if (mp3.available())
+  {
+    Serial.println(decodeMP3Answer());
+  }
+  delay(100);
   
 
 }
