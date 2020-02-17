74369141809c58427836049b6e1aeb145fc4bb42
Eric-Jimenez <56313440+Eric-Jimenez@users.noreply.github.com>
2020-02-17 12:38:42 -0600

    Codigo hola mundo para probar que todo funcione

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hola mundo");
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
+  
+
 }
