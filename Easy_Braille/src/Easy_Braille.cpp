0b22af3e022b7e84fdcc1be294218fc78dcb241d
ramirezo <oscar.rhernandez@alumnos.udg.mx>
2020-02-25 18:41:23 -0600

    Entradas AIN

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -1,11 +1,19 @@
 #include <Arduino.h>
 
+#define AIN1 27
+#define AIN2 26
+#define AIN3 25
+#define AIN4 33
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(2, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN3, OUTPUT);
+  pinMode(AIN4, OUTPUT);
 }
 
 void loop() {
