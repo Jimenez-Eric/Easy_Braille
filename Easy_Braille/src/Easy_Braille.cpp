c201c0c876efa9a44fd2ec0737bd1d89132c18ca
Eric Jimenez <61160495+Jimenez-Eric@users.noreply.github.com>
2020-06-04 12:56:55 -0500

    Se implemento la funcion de cambiar de letra y la funcion de subir o bajar el volumen utilizando los mismos botones

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -2,6 +2,17 @@
 #include <serial_mp3.h>
 #include <abecedario.h>
 
+#define nexButton 5
+#define backButton 15
+
+void volOrchangue();
+
+int nexButtonPressed = 0;
+int backButtonPressed = 0;
+
+int timePressed = 0;
+bool nexPressed = false;
+bool backPressed = false;
 
 
 void setup() {
@@ -13,8 +24,13 @@ void setup() {
   mp3.begin(9600);
   delay(500);
  
-  sendCommand(CMD_SEL_DEV, DEV_TF);
+  //sendCommand(CMD_SEL_DEV, DEV_TF);
   delay(500);
+
+  pinMode(nexButton, INPUT_PULLUP);
+  pinMode(backButton, INPUT_PULLUP);
+
+  //sendMP3Command('p');
 }
 
 void loop() {
@@ -23,7 +39,7 @@ void loop() {
   //sendMP3Command('p');  // con esta funcion se puede mandar el parametro para reproducir sin meter nada al serial
   //delay(1000);
 
-  char c = ' ';
+/*  char c = ' ';
  
   // If there a char on Serial call sendMP3Command to sendCommand
   if ( Serial.available() )
@@ -37,7 +53,83 @@ void loop() {
   {
     Serial.println(decodeMP3Answer());
   }
-  delay(100);
+  delay(100);*/
+
+  volOrchangue();
+  
+  
   
 
+  
+
+}
+
+
+void volOrchangue(){
+
+  /*Esta funcion sirve para los botones de siguiente y volumen si se deja presionado por mas de 1s el volumen bajara o subira
+  si es un solo pulso cambiara la letra hacia adelante o hacia atras, segun el boton presionado*/
+
+  nexButtonPressed = digitalRead(nexButton);
+  backButtonPressed = digitalRead(backButton);
+
+  while(nexButtonPressed == 0 || backButtonPressed == 0) 
+  {
+    timePressed++;
+    //Serial.println("Estoy en el while");
+    //Serial.println(timePressed);
+    nexButtonPressed = digitalRead(nexButton);
+    backButtonPressed = digitalRead(backButton);
+    if (nexButtonPressed == 0)
+    {
+      nexPressed = true;
+    }
+    else if (backButtonPressed == 0)
+    {
+      backPressed = true;
+    }
+    delay(20);
+ 
+  }
+  
+
+  if (nexPressed | backPressed)
+  {
+    if (timePressed < 15)
+    {
+      if (nexPressed)
+      {
+        sendMP3Command('>');
+        //Serial.println("Envie >");
+        timePressed = 0;
+        nexPressed = false;
+      }
+      else if (backPressed)
+      {
+        sendMP3Command('<');
+        //Serial.println("Envie <");
+        timePressed = 0;
+        backPressed = false;
+      }
+      
+      
+    }
+    else
+    {
+      if (nexPressed)
+      {
+        sendMP3Command('+');
+        //Serial.println("Envie +");
+        timePressed = 0;
+        nexPressed = false;
+      }
+      else if (backPressed)
+      {
+        sendMP3Command('-');
+        //Serial.println("Envie -");
+        timePressed = 0;
+        backPressed = false;
+      }
+    }
+  }
 }
