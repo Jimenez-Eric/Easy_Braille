95bd7a284e3fa5a04e990318f332a93c4ae7e55e
Bryan Angel Inda Valdivia <61247232+B-Angel620@users.noreply.github.com>
2020-06-03 23:12:37 -0500

    test 1

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -4,7 +4,9 @@
 #define AIN2 26
 #define AIN3 25
 #define AIN4 33
-
+#define Enter 38
+#define izquierda 39
+#define derecha 40
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,15 +16,37 @@ void setup() {
   pinMode(AIN2, OUTPUT);
   pinMode(AIN3, OUTPUT);
   pinMode(AIN4, OUTPUT);
+  pinMode(Enter, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hola mundo");
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
-  
 
+  //Menu de bienvenida.
+
+  //Sonidito de bienbvenida.
+
+  //Continuar presiona Enter.
+  while(Enter==1){
+
+  delay(5000);
+  //Para acceder al modod tutor presiona el boton izq
+  if(izquierda==1){
+    //TUTOR
+  }
+
+
+  if(derecha==1){
+    //AUTONOMO
+  }
+//puto biejoto
+  }
+  // put your main code here, to run repeatedly:
 }
+/*menu puede ser con la estructura que quieras ya sea while switch if 
+primero una bienvenida ya sea que la hagamos por audio cuando esta arrancando (grabar un mensaje de bienvenida)
+
+Modo autonomo o Modo Tutor 
+si seleccionas autonomo que te mande al menu de autonomo donde podras saltar las letras preconfiguradas con los botones es decir que empieze con la bocal A y que te preguinte si la
+quieres repetir o pasar a la siguiente y asi sucesivamente
+si seleccionas modo tutor se pondra a la escucha de la app que se va a desarrollar es decir que ahora la app tomara el control del hardware es decir con la app le dices que letra 
+mostrar*/
