44283edf652fa7a365dcb3114f798cc8922fea9d
ramirezo <oscar.rhernandez@alumnos.udg.mx>
2020-06-05 11:49:04 -0500

    encender y apagar bobinas

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -1,52 +1,134 @@
 #include <Arduino.h>
 
-#define AIN1 27
-#define AIN2 26
-#define AIN3 25
-#define AIN4 33
-#define Enter 38
-#define izquierda 39
-#define derecha 40
+#define AIN1 13
+#define AIN2 4
+#define AIN3 27
+#define AIN4 26
+
+#define BIN1 25
+#define BIN2 33
+#define BIN3 32
+#define BIN4 23
+ 
+#define CIN1 22
+#define CIN2 21
+#define CIN3 19
+#define CIN4 18
+
+#define tiempo 40
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(AIN3, OUTPUT);
   pinMode(AIN4, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(BIN3, OUTPUT);
+  pinMode(BIN4, OUTPUT);
+  pinMode(CIN1, OUTPUT);
+  pinMode(CIN2, OUTPUT);
+  pinMode(CIN3, OUTPUT);
+  pinMode(CIN4, OUTPUT);
+  
   pinMode(Enter, INPUT);
 }
 
 void loop() {
-
-  //Menu de bienvenida.
-
-  //Sonidito de bienbvenida.
-
-  //Continuar presiona Enter.
-  while(Enter==1){
-
-  delay(5000);
-  //Para acceder al modod tutor presiona el boton izq
-  if(izquierda==1){
-    //TUTOR
-  }
-
-
-  if(derecha==1){
-    //AUTONOMO
-  }
-//puto biejoto
-  }
   // put your main code here, to run repeatedly:
-}
-/*menu puede ser con la estructura que quieras ya sea while switch if 
-primero una bienvenida ya sea que la hagamos por audio cuando esta arrancando (grabar un mensaje de bienvenida)
 
-Modo autonomo o Modo Tutor 
-si seleccionas autonomo que te mande al menu de autonomo donde podras saltar las letras preconfiguradas con los botones es decir que empieze con la bocal A y que te preguinte si la
-quieres repetir o pasar a la siguiente y asi sucesivamente
-si seleccionas modo tutor se pondra a la escucha de la app que se va a desarrollar es decir que ahora la app tomara el control del hardware es decir con la app le dices que letra 
-mostrar*/
+
+
+
+
+}
+//***************************************ENCENDER BOBINAS******************************************************
+void on_bob1(){
+  Serial.println("onbob1");
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  delay(tiempo);
+  digitalWrite(AIN2,LOW);
+  
+}
+void on_bob2(){
+  Serial.println("onbob2");
+  digitalWrite(AIN3,LOW);
+  digitalWrite(AIN4,HIGH);
+  delay(tiempo);
+  digitalWrite(AIN4,LOW);
+}
+void on_bob3(){
+  Serial.println("onbob3");
+  digitalWrite(BIN1,LOW);
+  digitalWrite(BIN2,HIGH);
+  delay(20);
+  digitalWrite(BIN2,LOW);
+}
+void on_bob4(){
+  Serial.println("onbob4");
+  digitalWrite(BIN3,LOW);
+  digitalWrite(BIN4,HIGH);
+  delay(20);
+  digitalWrite(BIN4,LOW);
+}
+void on_bob5(){
+  Serial.println("onbob5");
+  digitalWrite(CIN1,LOW);
+  digitalWrite(CIN2,HIGH);
+  delay(20);
+  digitalWrite(CIN2,LOW);
+}
+void on_bob6(){
+  Serial.println("onbob6");
+  digitalWrite(CIN3,LOW);
+  digitalWrite(CIN4,HIGH);
+  delay(20);
+  digitalWrite(CIN4,LOW);
+}
+//***************************************APAGAR BOBINAS**********************************************
+void off_bob1 (){
+  Serial.println("offbob1");
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  delay(tiempo);
+  digitalWrite(AIN1,LOW);
+}
+void off_bob2 (){
+  Serial.println("offbob2");
+  digitalWrite(AIN3,HIGH);
+  digitalWrite(AIN4,LOW);
+  delay(tiempo);
+  digitalWrite(AIN3,LOW);
+}
+void off_bob3 (){
+  Serial.println("offbob3");
+  digitalWrite(BIN1,HIGH);
+  digitalWrite(BIN2,LOW);
+  delay(80);
+  digitalWrite(BIN1,LOW);
+}
+void off_bob4 (){
+  Serial.println("offbob4");
+  digitalWrite(BIN3,HIGH);
+  digitalWrite(BIN4,LOW);
+  delay(20);
+  digitalWrite(BIN3,LOW);
+}
+void off_bob5 (){
+  Serial.println("offbob5");
+  digitalWrite(CIN1,HIGH);
+  digitalWrite(CIN2,LOW);
+  delay(20);
+  digitalWrite(CIN1,LOW);
+}
+void off_bob6 (){
+  Serial.println("offbob6");
+  digitalWrite(CIN3,HIGH);
+  digitalWrite(CIN4,LOW);
+  delay(20);
+  digitalWrite(CIN3,LOW);
+}
+
