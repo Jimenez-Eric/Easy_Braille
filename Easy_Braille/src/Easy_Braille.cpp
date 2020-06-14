1e2e74bbacec8bb41f01bd0632bdc9a80a2de52a
Bryan Angel Inda Valdivia <61247232+B-Angel620@users.noreply.github.com>
2020-06-13 23:18:42 -0500

    WiFi server funcionando

--- Easy_Braille/src/Easy_Braille.cpp
+++ Easy_Braille/src/Easy_Braille.cpp
@@ -2,6 +2,7 @@
 
 #include "abecedario.h"
 #include "serial_mp3.h"
+#include <WiFi.h>
 
 abecedario miabecedario;
 serial_mp3 miMp3;
@@ -20,6 +21,7 @@ void nextBack();
 void readButtons();
 void soloMode();
 void tutorMode();
+void servidor();
 
 int enterButtonPressed = 1;
 int nexButtonPressed = 1;
@@ -30,6 +32,22 @@ int letra = 0;
 bool enterPressed = false;
 bool soloModeEnter = false;
 bool tutorModeEnter = false;
+bool iniciarServidor = false;
+
+// Replace with your network credentials
+const char* ssid = "TPlay";
+const char* password = "QLA620olw123987";
+// Set web server port number to 80
+WiFiServer server(80);
+// Variable to store the HTTP request
+String header;
+// Auxiliar variables to store the current output state
+String ledInterno2 = "off";
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 
 void setup()
@@ -44,6 +62,22 @@ void setup()
   pinMode(nexButton, INPUT_PULLUP);
   pinMode(backButton, INPUT_PULLUP);
   pinMode(enterButton, INPUT_PULLUP);
+  
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
   
 }
 
@@ -221,6 +255,7 @@ void tutorMode()
   timeEnterPressed = 0;
 
   readButtons();
+  servidor();
   while (enterButtonPressed == 0)
   {
     readButtons();
@@ -232,9 +267,12 @@ void tutorMode()
   {
     Serial.println("Estoy en modo tutor");
     tutorModeEnter = true;
+    iniciarServidor = true;
   }
   if (timeEnterPressed >= 500)
   {
+    iniciarServidor = false;
+    //server.close();
     timeEnterPressed = 0;
     state = 1;
     enterPressed = false;
@@ -273,3 +311,213 @@ void soloMode()
   volOrchangue();
 
 }
+
+void servidor()
+{
+  if(iniciarServidor){
+    
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client)
+  { // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client."); // print a message out in the serial port
+    String currentLine = "";       // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime)
+    { // loop while the client's connected
+      currentTime = millis();
+      if (client.available())
+      {                         // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out the serial monitor
+        header += c;
+        if (c == '\n')
+        { // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)
+          {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /A/on") >= 0)
+            {
+              Serial.println("Server letra A");
+               miabecedario.letra_A();
+            }
+            else if (header.indexOf("GET /B/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /C/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /D/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /E/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /F/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /G/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /H/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /I/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /J/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /K/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+
+            }
+            else if (header.indexOf("GET /L/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /M/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /N/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /O/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /P/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /Q/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /R/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /S/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /T/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /U/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /V/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /W/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /X/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+            else if (header.indexOf("GET /Y/on") >= 0)
+            {
+              Serial.println("GPIO 2 on");
+            }
+            else if (header.indexOf("GET /Z/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+            }
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+
+            // Web Page Heading
+            client.println("<font color=#bfe3ff><body> <h1>EasyBraile</h1> </font>");
+            //client.println("<body><h1>EasyBraile</h1>");
+            /****************************************************************/
+            client.println("<body style=\"background-color:black;\">");
+            // client.println("<font color=#bfe3ff> El viejito es joto </font>");
+            // client.println("<div style='border:2px solid ##FFFFFF;'>Borde con color #000080</div>");
+
+            // Display current state, and ON/OFF buttons for GPIO 26
+            /*************/ //client.println("<p>GPIO 26 - State " + ledInterno2 + "</p>");
+            // If the ledInterno2 is off, it displays the ON button
+
+            /*  
+           *  if (ledInterno2=="off") {
+              client.println("<p> <a href=\"/1/on\"><button class=\"button\">A</button></a>                <a href=\"/2/on\"><button class=\"button\">B</button></a>                <a href=\"/3/on\"><button class=\"button\">C</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/1/off\"><button class=\"button button2\">A</button></a>     <a href=\"/2/off\"><button class=\"button \">B</button></a>     <a href=\"/3/off\"><button class=\"button \">C</button></a></p>");
+            } 
+              */
+            // Display current state, and ON/OFF buttons for GPIO 27
+            /***************/ //client.println("<p>GPIO 27 - State " + output27State + "</p>");
+
+            /*  
+            *   If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            */
+            client.println("</body></html>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          }
+          else
+          { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {                   // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+    }
+  }
+  }
